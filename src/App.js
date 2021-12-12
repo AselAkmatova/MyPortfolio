f8e60b1cf695742cc782b7b2009efef3692ca704
AselAkmatova <aselakmatova20kb@gmail.com>
2021-12-12 20:00:23 +0600

    add responsive style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
+import "./styles/Responsive.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
