7b182e5c76982ac2fd036667c7611e97bd836e42
AselAkmatova <aselakmatova20kb@gmail.com>
2021-12-27 22:48:39 +0600

    update homepage responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,38 +11,41 @@ export default function Home() {
   return (
     <>
       <section className="home">
-        <div className="home-cover"></div>
-        <div className="home-info">
-          <Animated animationIn="slideInLeft" isVisible={true}>
-            <h1 className="home-info__title">Asel AKMATOVA</h1>
-          </Animated>
-          <Animated animationIn="slideInRight" isVisible={true}>
-            <h3 className="home-info__subtitle">Front-End Intern</h3>
-          </Animated>
-          <a href={file} download className="home-info__btn">
-            Download CV
-          </a>
-        </div>
-        <Swiper
-          className="home-carousel"
-          effect={"fade"}
-          autoplay={true}
-          loop={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-        >
-          <SwiperSlide>
-            <div className="home-carousel__slide home-carousel__slide-first"></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="home-carousel__slide home-carousel__slide-second"></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="home-carousel__slide home-carousel__slide-third"></div>
-          </SwiperSlide>
-        </Swiper>
+        <article className="home-cover">
+          <div className="home-info">
+            <Animated animationIn="slideInLeft" isVisible={true}>
+              <h1 className="home-info__title">Asel AKMATOVA</h1>
+            </Animated>
+            <Animated animationIn="slideInRight" isVisible={true}>
+              <h3 className="home-info__subtitle">Front-End Intern</h3>
+            </Animated>
+            <Animated animationIn="slideInLeft" isVisible={true}>
+              <a href={file} download className="home-info__btn">
+                Download CV
+              </a>
+            </Animated>
+          </div>
+          <Swiper
+            className="home-carousel"
+            effect={"fade"}
+            autoplay={true}
+            loop={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+          >
+            <SwiperSlide>
+              <div className="home-carousel__slide home-carousel__slide-first"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-carousel__slide home-carousel__slide-second"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-carousel__slide home-carousel__slide-third"></div>
+            </SwiperSlide>
+          </Swiper>
+        </article>
       </section>
     </>
   );
