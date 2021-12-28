32d3d22163070a07652bb66869efd45a7d91fbe2
AselAkmatova <aselakmatova20kb@gmail.com>
2021-12-28 21:25:51 +0600

    change savetheales styles

--- src/components/PortfolioModalTwo.js
+++
@@ -1,3 +0,0 @@
-export default function PortfolioModalTwo({ modal }) {
-  return <div className={` modal-two ${modal}`}>Modal Two</div>;
-}
--- src/components/PortfolioSaveTheAles.js
+++ src/components/PortfolioSaveTheAles.js
@@ -6,47 +6,59 @@ import image5 from "../images/saveales-5.png";
 import image6 from "../images/saveales-6.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css";
-import SwiperCore, { Navigation } from "swiper";
-SwiperCore.use([Navigation]);
+import "swiper/components/thumbs/thumbs.min.css";
+import SwiperCore, { Thumbs } from "swiper";
+import { useState } from "react";
+SwiperCore.use([Thumbs]);
 
 export default function PortfolioSaveTheAles() {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   return (
     <>
-      <article className="save-the-ales">
-        <Swiper navigation={true}>
-          <SwiperSlide>
-            <img src={image1} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image2} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image3} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image4} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image5} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image6} />
-          </SwiperSlide>
-        </Swiper>
-
-        <p className="save-the-ales__text">
-          This is an online food ordering app made for the bar
-          <b className="save-the-ales__title"> Save The Ales.</b> <br />{" "}
-          Realized at the end of IT-Academy course
-        </p>
-        <a
-          className="save-the-ales__btn"
-          target="_blank"
-          href="https://github.com/AselAkmatova/SaveTheAles-2021"
-        >
-          View on GitHub
-        </a>
+      <article className="savetheales">
+        <div className="savetheales-carousel">
+          <Swiper
+            loop={true}
+            spaceBetween={10}
+            thumbs={{ swiper: thumbsSwiper }}
+            className="mySwiper2"
+          >
+            <SwiperSlide className="slider-main__slide slider-main__slide-one"></SwiperSlide>
+            <SwiperSlide className="slider-main__slide slider-main__slide-two"></SwiperSlide>
+            <SwiperSlide className="slider-main__slide slider-main__slide-three"></SwiperSlide>
+            <SwiperSlide className="slider-main__slide slider-main__slide-four"></SwiperSlide>
+            <SwiperSlide className="slider-main__slide slider-main__slide-five"></SwiperSlide>
+          </Swiper>
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            loop={true}
+            spaceBetween={10}
+            slidesPerView={5}
+            watchSlidesProgress={true}
+            className="mySwiper"
+          >
+            <SwiperSlide className="slider-small__slide slider-small__slide-one"></SwiperSlide>
+            <SwiperSlide className="slider-small__slide slider-small__slide-two"></SwiperSlide>
+            <SwiperSlide className="slider-small__slide slider-small__slide-three"></SwiperSlide>
+            <SwiperSlide className="slider-small__slide slider-small__slide-four"></SwiperSlide>
+            <SwiperSlide className="slider-small__slide slider-small__slide-five"></SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="savetheales-info">
+          <p className="savetheales-info__text">
+            This is an online food ordering app made for the bar
+            <b> Save The Ales.</b> Realized at the end of IT-Academy course. You
+            can check it on{" "}
+            <a
+              className="savetheales-info__link"
+              target="_blank"
+              href="https://github.com/AselAkmatova/SaveTheAles-2021"
+            >
+              GitHub
+            </a>
+          </p>
+        </div>
       </article>
     </>
   );
